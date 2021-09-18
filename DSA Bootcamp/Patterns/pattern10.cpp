ccde6344dc71f50aade6c663b99d1cffb61f463a
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-09-18 15:00:10 +0530

    pattern 9 and 10

---
+++ DSA Bootcamp/Patterns/pattern10.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n-i;j++)
+            cout<<" ";
+        for(int k=1;k<=i;k++)
+            cout<<"* ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+}
---
+++ DSA Bootcamp/Patterns/pattern11.cpp
@@ -0,0 +1,20 @@
+
+#include<iostream>
+using namespace std;
+void pattern(int n)
+{
+    for(int i=n;i>0;i--)
+    {
+        for(int j=n-i;j>0;j--)
+            cout<<" ";
+        for(int k=1;k<=i;k++)
+            cout<<"* ";
+        cout<<"\n";
+    }
+}
+int main()
+{
+    int n;
+    cin>>n;
+    pattern(n);
+}
