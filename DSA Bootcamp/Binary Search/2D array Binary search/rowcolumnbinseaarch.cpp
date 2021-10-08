39d46309e71f1b761f2bfc59b28c243d9b47bbe7
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-09 02:06:51 +0530

    rowcolumn2d added

--- DSA Bootcamp/Binary Search/2D array Binary search/rowcolumnbinseaarch.cpp
+++ DSA Bootcamp/Binary Search/2D array Binary search/rowcolumnbinseaarch.cpp
@@ -1,42 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {
     int n;
-    cin>>n;
+    cin >> n;
     int ar[n][n];
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        for(int j=0;j<n;j++)
+        for (int j = 0; j < n; j++)
         {
-            cin>>ar[i][i];
+            cin >> ar[i][i];
         }
     }
     int target;
-    cin>> target;
-    int r=0,c=n-1;
+    cin >> target;
+    int r = 0, c = n - 1;
     // vector<int> v(2,-1);
-    int count=0;
-    while (r<n and c>=0)
+    int count = 0;
+    while (r < n and c >= 0)
     {
-        if(ar[r][c]==target)
+        if (ar[r][c] == target)
         {
-            count=1;
-            cout<<r<<" "<<c<<endl;
+            count = 1;
+            cout << r << " " << c << endl;
             break;
         }
-        else if(ar[r][c]<target)
+        else if (ar[r][c] < target)
         {
             r++;
         }
-        else{
+        else
+        {
             c--;
         }
     }
 
-    if(count==0)
-    cout<<-1<<" "<<-1<<endl;
+    if (count == 0)
+        cout << -1 << " " << -1 << endl;
 }
 
 /*
