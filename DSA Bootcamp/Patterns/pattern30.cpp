ea96e2adf8e5d61e808c528b46e23f3c377294c1
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-15 20:43:49 +0530

    pattern files added

---
+++ DSA Bootcamp/Patterns/pattern30.cpp
@@ -0,0 +1,30 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int  j = 1; j <= n-i; j++)
+        {
+            cout<<"   ";
+        }
+        int c=1;
+        for(int k=1;k<=i;k++)
+        {
+            cout<<" "<<c++<<" ";
+        }
+        if(i>1){
+        for (int k = 2; k <= i; k++)
+        {
+            cout<<" "<<k<<" ";
+        }
+        }
+        cout<<endl;        
+    }
+    
+}
