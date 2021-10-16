246779a6126a478925ef495fb940336264970e67
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-16 22:15:35 +0530

    pattern 17 added

---
+++ DSA Bootcamp/Patterns/pattern17.cpp
@@ -0,0 +1,50 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        for (int  j = 1; j <= n-i; j++)
+        {
+            cout<<"   ";
+        }
+        int c=i;
+        for(int k=1;k<=i;k++)
+        {
+            cout<<" "<<c--<<" ";
+        }
+        if(i>1){
+        for (int k = 2; k <= i; k++)
+        {
+            cout<<" "<<k<<" ";
+        }
+        }
+        cout<<endl; 
+    }  
+    for (int i = n-1; i >=0; i--)
+    {
+        for (int  j = 1; j <= n-i; j++)
+        {
+            cout<<"   ";
+        }
+        int c=i;
+        for(int k=1;k<=i;k++)
+        {
+            cout<<" "<<c--<<" ";
+        }
+        if(i>1){
+        for (int k = 2; k <= i; k++)
+        {
+            cout<<" "<<k<<" ";
+        }
+        }
+        cout<<endl;
+             
+    }
+
+}
