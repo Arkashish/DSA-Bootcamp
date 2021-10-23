ce0c2e0c746f4d5c69258c0748b8d3c3c0182d7d
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-23 13:34:40 +0530

    Magic Number added

---
+++ DSA Bootcamp/Bitwise and Number Theory/magicnumber.cpp
@@ -0,0 +1,19 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n=6;
+    int base = 5;
+    int ans= 0;
+    while (n>0)
+    {
+        int last = n & 1;
+        n = n >> 1;
+        ans+=last*base;
+        base*=5;
+    }
+    cout<<ans<<endl;  
+
+}
