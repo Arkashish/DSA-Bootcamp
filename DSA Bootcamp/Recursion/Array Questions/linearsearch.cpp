ac1cf44abe4e4490064cdb5a747af513bc86b9d8
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-28 21:58:56 +0530

    Linear Search with index

--- DSA Bootcamp/Recursion/Array Questions/linearsearch.cpp
+++ DSA Bootcamp/Recursion/Array Questions/linearsearch.cpp
@@ -18,12 +18,28 @@ bool targetfinder_linearsearchbyk(int *arr,int target,int i)
     return false;
 
     
-    return (arr[i]==target) or targetfinder_linearsearch(arr,target,i+1);;
+    return (arr[i]==target) or targetfinder_linearsearch(arr,target,i+1);
     
+}
+//to return the aarray indices with repitition in a vector
+vector<int> linesearch(int *arr,int target,int i)
+{
+    static vector<int> v;
+    if(i==8)
+    return v;
+
+    if(arr[i]==target)
+    v.push_back(i);
+
+    vector<int> ans = linesearch(arr,target,i+1);
+    return ans;
 }
 int main()
 {
     int arr[]={1,2,4,6,18,2,29,0};
     cout<<targetfinder_linearsearch(arr,18,0)<<endl;
-    cout<<targetfinder_linearsearchbyk(arr,18,0);
+    cout<<targetfinder_linearsearchbyk(arr,18,0)<<endl;
+    vector<int> v = linesearch(arr,2,0);
+    for(int i=0;i<v.size();i++)
+        cout<<v[i]<<" ";
 }
