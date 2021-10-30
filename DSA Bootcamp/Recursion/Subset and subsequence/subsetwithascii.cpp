fb02a4035ca31255c6b7e6861d19d6421a04d1f2
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-10-31 01:21:23 +0530

    Need to revsisit the ascii part is not worrking'

---
+++ DSA Bootcamp/Recursion/Subset and subsequence/subsetwithascii.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void subsetwithascii(string str,string temp)
+{
+    if(str.size()==0)
+    {
+        cout<<temp<<endl;
+        return;
+    }
+    // char ch = str[i];
+    subsetwithascii(str.substr(1),temp+str[0]);
+    subsetwithascii(str.substr(1),temp);
+    subsetwithascii(str.substr(1),temp+(char)(str[0]+(char)0));    
+    subsetwithascii(str.substr(1),temp+str[0]+char('0'));    
+}
+void subset(string str,string temp)
+{
+    if(str.size()==0)
+    {
+        cout<<temp<<endl;
+        return;
+    }
+    // char ch = str[i];
+    subset(str.substr(1),temp+str[0]);
+    subset(str.substr(1),temp);
+    // subsetwithascii(str,i+1,temp);
+}
+int main()
+{
+    string str = "abc";
+    subsetwithascii(str,"");
+}
