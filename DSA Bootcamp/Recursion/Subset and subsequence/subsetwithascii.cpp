4b53013d6f871e67d00807b074f920f9a9242778
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2021-11-01 12:29:52 +0530

    Subset with ASCII printing finally doneee

--- DSA Bootcamp/Recursion/Subset and subsequence/subsetwithascii.cpp
+++ DSA Bootcamp/Recursion/Subset and subsequence/subsetwithascii.cpp
@@ -1,33 +1,37 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void subsetwithascii(string str,string temp)
+void subsetwithascii(string str, string temp)
 {
-    if(str.size()==0)
+    if (str.size() == 0)
     {
-        cout<<temp<<endl;
+        cout << temp << endl;
         return;
     }
     // char ch = str[i];
-    subsetwithascii(str.substr(1),temp+str[0]);
-    subsetwithascii(str.substr(1),temp);
-    subsetwithascii(str.substr(1),temp+(char)(str[0]+(char)0));    
-    // subsetwithascii(str.substr(1),temp+str[0]+char(str[0]+0));    
+    subsetwithascii(str.substr(1), temp + str[0]);
+    subsetwithascii(str.substr(1), temp);
+    char ch = str[0] + 0;
+    // cout<< " ["<<ch<<"] "<<endl;
+    int num = (int)(ch);
+    // string ans = temp + str[0];
+    subsetwithascii(str.substr(1),temp+str[0]+to_string(num));
+    // subsetwithascii(str.substr(1),temp+str[0]+char(str[0]+0));
 }
-void subset(string str,string temp)
+void subset(string str, string temp)
 {
-    if(str.size()==0)
+    if (str.size() == 0)
     {
-        cout<<temp<<endl;
+        cout << temp << endl;
         return;
     }
     // char ch = str[i];
-    subset(str.substr(1),temp+str[0]);
-    subset(str.substr(1),temp);
+    subset(str.substr(1), temp + str[0]);
+    subset(str.substr(1), temp);
     // subsetwithascii(str,i+1,temp);
 }
 int main()
 {
     string str = "abc";
-    subsetwithascii(str,"");
+    subsetwithascii(str, "");
 }
